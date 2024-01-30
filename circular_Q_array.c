6420900d3a0d29ae0ea49edcb8ac6f1c4a9324ff
SUJAN GANIGA <sujan.ganiga2020@gmail.com>
2024-01-30 12:37:09 +0530

    30-01-2024

--- circular_Q_array.c
+++ circular_Q_array.c
@@ -5,21 +5,22 @@
 int queue[size];
 int f=-1,r=-1;
 void enqueue(int x){
-    if(r==size-1){
+    if((r==size-1&&f==0)||f==r+1){
         printf("queue is full");
     }
     else{
         if(f==-1){
             f=0;
         }
-        r++;
+        r=(r+1)%size;
         queue[r]=x;
 
     }
 }
 void dequeue(){
     if(f==-1||f>r){
-        printf("queue is empty");
+        printf("queue is empty\n");
+
     }
     else{
         int v=queue[f];
@@ -27,6 +28,9 @@ void dequeue(){
         if(f>r){
             f=r=-1;
         }
+        printf("\ndeleted element is \t %d",v);
+        printf("\n REMAINING ARRAY IS :: \n");
+        show();
     }
 }
 void show(){
@@ -34,30 +38,33 @@ void show(){
         printf("queue is empty");
     }
     else{
-        for(int i=f;i<=r;i++){
+        int i;
+        for(i=f;i!=r;i=(i+1)%size){
             printf("%d \t",queue[i]);
         }
+        printf("%d \t",queue[i]);
+
     }
 }
 void peek(){
     if(f==-1){
-        printf("queue is empty");
+        printf("queue is empty\n");
     }
     else{
-        printf("OUT PUT IS : \n");
+        printf("\n OUT PUT IS : \n");
         printf("%d",queue[f]);
     }
 }
 
 int main(){
-    printf("\n 1. enqueue \n 2. dequeue \n 3. peek \n 4. show \n 5. out\n enter option : \t");
+        printf("\n 1. enqueue \n 2. dequeue \n 3. peek \n 4. show \n 5. out\n enter option : \t");
     int ch,x;
     scanf("%d",&ch);
     while(1){
         
     switch(ch){
         case 1:
-            printf("enter the element yo insert");
+            printf("enter the element to insert");
             scanf("%d",&x);
             enqueue(x);
             break;
@@ -74,7 +81,8 @@ int main(){
             exit (0);
             break;
     }
-    printf("\n \n 1. enqueue \n 2. dequeue \n 3. peek \n 4. show \n 5. out \n  enter option : \t");
+    printf("\n 1. enqueue \n 2. dequeue \n 3. peek \n 4. show \n 5. out\n enter option : \t");
+
         scanf("%d",&ch);
     
     }
